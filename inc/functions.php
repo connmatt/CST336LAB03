768294bf34e4615ce1153e7d280ae86753858688
Eric Tinoco <ertinoco@csumb.edu>
2017-09-28 00:24:03 +0000

    test

--- inc/functions.php
+++ inc/functions.php
@@ -19,6 +19,12 @@
         
     }
     
+    function setPoints($deck){
+        
+    }
+    
+    
+    
     function createDeck(){
         //declare an array for the entire deck max 52 cards, min 1
         $deck = array();
@@ -37,21 +43,17 @@
             array_push($suit, ${"spades". ($i+1)});
         }
         
-        // for($i = 0; $i < count($deck); $i++){
-        //     echo $deck[$i];
-        // }
-        
-        
-        
         foreach($suit as $card){
             foreach($card as $key => $value){
                 array_push($deck, $value);
             }
         }
         
-        for($i = 0; $i < count($deck); $i++){
-            echo $deck[$i];
-        }
+        echo $deck[14];
+        echo $deck[23];
+        echo $deck[1];
+        echo $deck[43];
+        
     }
     
     function play(){
@@ -59,31 +61,3 @@
     }
     
 ?>
-
-<!--$cards = array();-->
-            
-<!--            $card1["value"] = 10;-->
-<!--            $cards[] = $card1;-->
-            
-<!--            $card2["suit"] = "Hearts";-->
-<!--            $card2["value"] = 9;-->
-<!--            array_push($cards, $card2);-->
-            
-<!--            $card3["suit"] = "Hearts";-->
-<!--            $card3["value"] = 9;-->
-<!--            array_push($cards, $card2);-->
-            
-<!--            $card4["suit"] = "Hearts";-->
-<!--            $card4["value"] = 9;-->
-            
-<!--            array_push($cards, $card2);-->
-<!--            foreach($cards as $card) {-->
-<!--                foreach($card as $key => $value) {-->
-<!--                    echo "$key = $value<br />";-->
-<!--                }-->
-<!--            }-->
-            
-<!--            for ($i = 0; $i < 3; $i++) {-->
-<!--                echo "<br />";-->
-<!--                var_dump(array_keys($cards[0]));-->
-<!--            }-->
